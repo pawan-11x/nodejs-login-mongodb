67eb8b2acf85d0bf8402941f318fcae519905f52
pawan-11x <itz.pawan2213@gmail.com>
2026-03-16 01:12:53 +0530

    Added server file

---
+++ database_form.js
@@ -0,0 +1,38 @@
+
+console.log("Welcome to my Database");
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(__dirname));   // serve files from same folder
+
+mongoose.connect("mongodb://localhost:27017/mydb")
+.then(()=>console.log("MongoDB Connected"))
+.catch(err=>console.log(err));
+
+const schema = new mongoose.Schema({
+    username:String,
+    password:String
+});
+
+const User = mongoose.model("login",schema);
+
+app.post("/login",async(req,res)=>{
+    console.log(req.body);
+
+    const data = new User({
+        username:req.body.username,
+        password:req.body.password
+    });
+
+    await data.save();
+
+    res.send("Data saved successfully");
+
+});
+
+app.listen(5000,()=>{
+console.log("Server running on port 5000");
+});
